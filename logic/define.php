ac0abb74d017b89e4e6dc952ee5fe6ddfd2897ec
ymbyuki <ymbyuki0104@gmail.com>
2021-09-08 15:50:34 +0900

    bag fix and add degine

---
+++ logic/define.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST','us-cdbr-east-04.cleardb.com');
+define('DB_NAME','heroku_2b1b2eec4d034ec');
+define('DB_USER','b36d221f7d760a');
+define('DB_PASS','c397d807');
--- view/header.php
+++ view/header.php
@@ -9,26 +9,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">情報管理</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">ホーム</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="new.php">新規追加</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="addcontent.php">コンテンツ入力</a>
-                        </li>
-                    </ul>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">情報管理</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">ホーム</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="new.php">新規追加</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="addcontent.php">コンテンツ入力</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
+</header>
